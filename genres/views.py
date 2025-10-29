189fe657539cb5fb4d8d5299cbf5a2793faf5a94
Izaias Rios <izaias.rios@gmail.com>
2025-10-29 08:37:59 -0300

    Instalação do DRF e class GenreCreateListView.

--- genres/views.py
+++ genres/views.py
@@ -3,18 +3,29 @@ from django.http import JsonResponse
 from genres.models import Genre
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from genres.serializers import GenreSerializer
 
-@csrf_exempt
-def genre_create_list_view(request):
-    if request.method == 'GET':
-      genres = Genre.objects.all()
-      data = [{"id": genre.id, "name": genre.name} for genre in genres]
-      return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-     data = json.loads(request.body.decode('utf-8'))
-     new_genre = Genre(name=data['name'])
-     new_genre.save()
-     return JsonResponse({"id": new_genre.id, "name": new_genre.name}, status=201)
+
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+# @csrf_exempt
+# def genre_create_list_view(request):
+#     if request.method == 'GET':
+#       genres = Genre.objects.all()
+#       data = [{"id": genre.id, "name": genre.name} for genre in genres]
+#       return JsonResponse(data, safe=False)
+#     elif request.method == 'POST':
+#      data = json.loads(request.body.decode('utf-8'))
+#      new_genre = Genre(name=data['name'])
+#      new_genre.save()
+#      return JsonResponse({"id": new_genre.id, "name": new_genre.name}, status=201)
+    
+
 
 @csrf_exempt   
 def genre_detail_view(request, genre_id):
